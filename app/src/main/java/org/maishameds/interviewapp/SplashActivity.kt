e12a223348c071673644b44b18865986de0d1a2e
JobGetabu <getabujob@gmail.com>
2020-07-22 21:04:07 +0300

    initial commit

---
+++ app/src/main/java/org/maishameds/interviewapp/SplashActivity.kt
@@ -0,0 +1,18 @@
+package org.maishameds.interviewapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        //quick switch to main activity
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK and Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
+        finish()
+    }
+}
